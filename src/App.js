91f6c85909debf083b2421f488da747b853461fd
xxuegoo <diegonunez.dev@gmail.com>
2021-03-31 04:26:33 -0400

    adding changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import Letter from './components/Letter';
+import WordLength from './components/WordLength';
+import WordContainer from './components/WordContainer';
+import { useState } from 'react';
 
-function App() {
+const App = () => {
+  const alphabet = ["A", "B", "C", "D", "E", "F", "H", "G", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  const minWordLength = 2;
+  const maxWordLength = 15;
+  const [wordLength, setWordLength] = useState(minWordLength);
+
+  const makeLetters = () => {
+    return(
+      alphabet.map((letter) => {
+        return <Letter Letter={letter} />
+      })
+    )
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Scrabble Solver! {wordLength}
+        <div className="App-width">
+          {makeLetters()}
+        </div>
+        <WordLength
+          minWordLength={minWordLength}
+          maxWordLength={maxWordLength}
+          setWordLength={setWordLength}
+        />
+        <WordContainer />
       </header>
     </div>
   );
